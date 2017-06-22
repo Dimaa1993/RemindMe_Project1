3dd54c816b564f4ecd25ba5a86f9a3cf82a3e836
Dimaa1993 <dima.harmider93@gmail.com>
2017-06-22 22:56:00 +0300

    Created Main_Activity

---
+++ app/src/main/java/com/dima/rm/Main_Activity.java
@@ -0,0 +1,15 @@
+package com.dima.rm;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class Main_Activity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
